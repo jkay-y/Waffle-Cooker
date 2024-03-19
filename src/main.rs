fc264446ab64e4f1149cdd5ca2d43e5047fe88e7
Jun <juuyoun01@gmail.com>
2024-03-19 17:57:41 +1100

    Fetch Daily Waffle HTML; Parse a given Waffle's HTML and put into data structure function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::string;
+
 use scraper::{Html, Selector};
 use thirtyfour::prelude::*;
+use regex::Regex;
 use tokio::time::Duration;
 
 #[tokio::main]
@@ -48,31 +51,146 @@ async fn main() {
 
 }
 
-fn get_waffle_into(waffle_html: String) {
-    let page_doc = Html::parse_document(&waffle_html);
+struct WaffleBoard {
+    number: u32,
+    tiles: [[WaffleTile; 5]; 5],
+}
 
-    let daily_num_selector = Selector::parse("div.game-number").unwrap();
+impl WaffleBoard {
+    fn print_board(&self) {
+        for y in 0..5 {
+            for x in 0..5 {
+                match self.tiles[y][x].color {
+                    WaffleTileColor::Green => print!("G"),
+                    WaffleTileColor::Orange => print!("O"),
+                    WaffleTileColor::White => print!("W"),
+                    WaffleTileColor::None => print!("N"),
+                }
+                print!("{},", self.tiles[y][x].letter);
+            }
+            print!("\n");
+        }
+    }
+}
 
-    for element in page_doc.select(&daily_num_selector) {
-        if !element.inner_html().is_empty() {
-            println!("{:?}", element.inner_html());
+fn new_board() -> WaffleBoard {
+    let temp_tile = WaffleTile {
+        letter: 'a',
+        color: WaffleTileColor::None,
+    };
+    let board: WaffleBoard = WaffleBoard {
+        number: 0,
+        tiles: [
+            [temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone()],
+            [temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone()],
+            [temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone()],
+            [temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone()],
+            [temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone(), temp_tile.clone()]
+        ],
+    };
+    return board;
+}
+
+#[derive(Copy, Clone)]
+struct WaffleTile {
+    letter: char,
+    color: WaffleTileColor,
+}
+
+impl WaffleTile {
+
+    fn set_color(&mut self, choice: WaffleTileColor) {
+        match choice {
+            WaffleTileColor::Green => {
+                if self.color != WaffleTileColor::Green {
+                    self.color = WaffleTileColor::Green;
+                }
+            },
+            WaffleTileColor::Orange => {
+                if self.color != WaffleTileColor::Orange {
+                    self.color = WaffleTileColor::Orange;
+                }
+            },
+            WaffleTileColor::White => {
+                if self.color != WaffleTileColor::White {
+                    self.color = WaffleTileColor::White;
+                }
+            },
+            _ => {
+                println!("ERROR - set_color() choice was not Green | Orange | White");
+            },
         }
     }
 
-    for x in 0..5 {
-        for y in 0..5 {
+    fn set_letter(&mut self, letter_choice: char) {
+        if !(self.letter == letter_choice) && letter_choice.is_ascii_uppercase() {
+            self.letter = letter_choice;
+        }
+    }
+
+}
+
+#[derive(Copy, Clone, PartialEq)]
+enum WaffleTileColor {
+    Green,
+    Orange,
+    White,
+    None
+}
+
+
+fn get_waffle_into(waffle_html: String) {
+
+    let page_doc = Html::parse_document(&waffle_html);
+
+    let mut board = new_board();
+
+    let daily_num_selector = Selector::parse("div.game-number").unwrap();
+    for element in page_doc.select(&daily_num_selector) {
+        if !element.inner_html().is_empty() {
+            let html_text = element.inner_html();
+            let num_re = Regex::new(r#"\d+"#).unwrap();
+            let num_captures = num_re.captures(&html_text).expect("No match found");
+            board.number = num_captures[0].parse().expect("Failed to parse Daily Waffle #");
+            println!("{:?}", board.number);
+        }
+    }
+
+    for y in 0..5 {
+        for x in 0..5 {
             if x % 2 != 0 && y % 2 != 0 {
                 continue;
             }
             let selector = format!(r#"div[data-pos*="{{\"x\":{},\"y\":{}}}"]"#, x, y);
             let tile_selector = Selector::parse(&selector).unwrap();
             for element in page_doc.select(&tile_selector) {
-                if !element.inner_html().is_empty() {
-                    println!("Pos ({x}, {y}) - {:?}", element.html());
+                let outer_html = element.html();
+                if !outer_html.is_empty() {
+                    let class_re = Regex::new(r#"class="tile draggable tile--[a-z]( green| yellow)?"#).unwrap();
+                    if class_re.is_match(&outer_html) {
+                        let class_captures = class_re.captures(&outer_html).expect("No match found");
+                        let mut class_name_attribute = class_captures[0].to_string();
+                        class_name_attribute = class_name_attribute[28..].to_string();
+                        board.tiles[y][x].set_letter(class_name_attribute.chars().nth(0).unwrap().to_ascii_uppercase());
+                        match class_name_attribute.len() {
+                            1 => {
+                                board.tiles[y][x].set_color(WaffleTileColor::White);
+                            },
+                            _ => {
+                                let check_for = class_name_attribute[2..].to_string().to_lowercase();
+                                if check_for == "green".to_string() {
+                                    board.tiles[y][x].set_color(WaffleTileColor::Green);
+                                } else if check_for == "yellow".to_string() {
+                                    board.tiles[y][x].set_color(WaffleTileColor::Orange);
+                                }
+                            },
+                        }
+                    }
                 }
             }
-
         }
     }
+
+    board.print_board();
 
 }
